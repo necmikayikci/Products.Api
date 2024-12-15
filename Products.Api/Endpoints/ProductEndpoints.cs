9838b055f59531cbf15b04cd9a9d9400a04fe46b
Necmi Kayıkcı <necmi.kayikci@vagani.com>
2024-12-15 18:47:24 +0300

    Add project files.

---
+++ Products.Api/Endpoints/ProductEndpoints.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Api.Contracts;
+using Products.Api.Database;
+using Products.Api.Entities;
+
+namespace Products.Api.Endpoints
+{
+    public static class ProductEndpoints
+    {
+        public static void MapProductEndpoints(this IEndpointRouteBuilder app)
+        {
+            //Create Product
+            app.MapPost("products", async (CreateProductRequest request, ApplicationDbContext context, CancellationToken cToken) =>
+            {
+                var product = new Product();
+                product.Name = request.Name;
+                product.Price = request.Price;
+
+                context.Add(product);
+
+                await context.SaveChangesAsync(cToken);
+
+                return Results.Ok(product);
+            });
+
+            //Update Product
+            app.MapPut("products/{id}", async (int id, UpdateProductRequest request, ApplicationDbContext context, CancellationToken cToken) =>
+            {
+                var product = await context.Products.FirstOrDefaultAsync(p => p.Id == id, cToken);
+
+                if (product is null)
+                {
+                    return Results.NotFound();
+                }
+
+                product.Name = request.Name;
+                product.Price = request.Price;
+
+                await context.SaveChangesAsync(cToken);
+
+                return Results.NoContent();
+            });
+
+            //Get Product
+            app.MapGet("products/{id}", async (int id, ApplicationDbContext context, CancellationToken cToken) =>
+            {
+                var product = await context.Products.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id, cToken);
+
+                return product is null ? Results.NotFound() : Results.Ok(product);
+            });
+
+            //Get Products with paging
+            app.MapGet("products", async (ApplicationDbContext context, CancellationToken ct, int page = 1, int pageSize = 10) =>
+            {
+                var products = await context.Products
+                    .AsNoTracking()
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync(ct);
+
+                return Results.Ok(products);
+            });
+
+            //Delete Product
+            app.MapDelete("products/{id}", async (int id, ApplicationDbContext context, CancellationToken ct) =>
+            {
+                var product = await context.Products.FirstOrDefaultAsync(p => p.Id == id, ct);
+
+                if (product is null)
+                {
+                    return Results.NotFound();
+                }
+
+                context.Remove(product);
+
+                await context.SaveChangesAsync(ct);
+
+                return Results.NoContent();
+            });
+        }
+    }
+}
---
+++ Products.Api/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Products.Api.Database;
+using Products.Api.Endpoints;
+using Products.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.ApplyMigrations();
+}
+
+app.MapProductEndpoints();
+
+app.Run();
